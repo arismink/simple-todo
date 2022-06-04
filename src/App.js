0b72f523283e312f887d60e5023704c60722f5e6
arismink <krismina.la@gmail.com>
2022-06-04 14:36:06 -0600

    add todo form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
+
+import TodoForm from "./components/TodoForm";
+
 import './App.css';
 
 function App() {
+  const [toDos, setToDos] = useState([]);
+
+  const addToDo = (toDo) => {
+    setToDos([...toDo, ...toDos])
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <p>React todo</p>
+        <TodoForm
+          addToDo={addToDo}
+        />
       </header>
     </div>
   );
---
+++ src/components/TodoForm.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+function TodoForm() {
+  const [toDo, setTodo] = useState({
+    id: "",
+    task: "",
+    completed: false,
+  });
+
+  const handleTaskInputChange = (e) => {
+    setTodo({...toDo, task: e.target.value})
+  }
+
+  return (
+    <form>
+      <input
+        name="task"
+        type="text"
+        value={toDo.task}
+        onChange={handleTaskInputChange}
+      />
+      <button />
+    </form>
+  );
+}
+
+export default TodoForm;
